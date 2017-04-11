806f5e6b5bb3940ab3b741bde3d8ef709dba3e13
yodfz <yodfz@qq.com>
2017-04-11 13:56:38 +0800

    增加微信小程序模版

--- src/page_template.js
+++ src/page_template.js
@@ -2,11 +2,29 @@
  * 页面级模版生成配置表
  */
 var $Data = {
+    // vue
     'vue': {
         files: [
             {
                 path:'/template/vue/template.vue',
                 name:'[name].vue'
+            }
+        ]
+    },
+    // 微信小程序
+    'wxml': {
+        files:[
+            {
+                path:'/template/weixin/mic_template.js',
+                name:'[name].js'
+            },
+            {
+                path:'/template/weixin/mic_template.wxml',
+                name:'[name].wxml'
+            },
+            {
+                path:'/template/weixin/mic_template.wxss',
+                name:'[name].wxss'
             }
         ]
     }
